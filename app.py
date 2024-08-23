505e2551279be6cc0868a9c2cd9f0abb3349dbda
sen-sourav <souravsen1990@gmail.com>
2024-08-23 10:07:49 -0500

    for deploy

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ def get_chord_from_roman(numeral, key):
     return keys[chord_index]
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5001)
